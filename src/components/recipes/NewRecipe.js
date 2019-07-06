1c4a3d2071115bdf339e34f3703cc2d392279bcb
Adam Ladell <adamladell@outlook.com>
2019-07-06 17:01:52 +1000

    Removed testing data

--- src/components/recipes/NewRecipe.js
+++ src/components/recipes/NewRecipe.js
@@ -10,7 +10,7 @@ class NewRecipe extends Component {
     super(props);
 
     this.state = {
-      ingredients: ['test'],
+      ingredients: [],
     };
 
     this.handleChange = this.handleChange.bind(this);
